f646582b2f44a32d1f184c47f0f2544573c6c225
Finallion <74151350+finallion@users.noreply.github.com>
2023-08-12 15:06:48 +0200

    updated read me

--- README.md
+++ README.md
@@ -1,5 +1,126 @@
-# VillagersPlus
+# VillagersPlus - a villager extension 
 
-## A villager extension 
+## Outline
+1. Introduction
+2. Datapack Support
+   1. Introduction
+   2. Replacing the default trades for VillagerPlus villagers
+   3. Replacing the default trades for Minecraft villagers
+   4. Adding trades to villagers
 
+
+## Introduction
 This mod adds new villager types, trades and unique and useful workstations.
+
+Currently added:
+- Horticulturist
+- Oceanographer
+- Alchemist
+- Occultist
+
+## Datapack Support
+### Introduction
+
+VillagerPlus provides full support of customizing trades for:
+- VillagerPlus Professions
+- Vanilla Villager Professions
+
+It not only allows to overwrite, modify or delete trades. VillagerPlus also provides more customization of trades.
+You can change the currency from emeralds to whatever item you like, for example.
+
+Further VillagersPlus is able to handle **multiple** datapacks modifying the same villager by merging its trades.
+
+### Replacing the default trades for VillagerPlus villagers
+Create a datapack with this directory structure:
+
+    data/villagersplus/default_villager_trades
+
+Copy the JSON-file from the VP-villager whose trades you'd like to modify into your datapack. <br>
+The default trade files are located in inside the jar-file under:
+
+    data/villagersplus/default_villager_trades
+
+Modify the trades in the JSON as you please. Your datapack JSON will overwrite the default JSON from the VP.
+
+### Replacing the default trades for Minecraft villagers
+_Notice: The vanilla trades aren't in JSON-form yet. So modifying those, is a bit more work._
+
+Create a datapack with this directory structure:
+
+    data/villagersplus/default_villager_trades
+
+Create a JSON with the name of villager-profession whose trades you'd like to modify. <br>
+For example:
+
+    butcher.json
+
+Below is the basic structure:
+    
+    {
+        "profession": "minecraft:butcher",
+        "trades": {
+            "novice": [
+        
+            ],
+            "apprentice": [
+        
+            ],
+            "journeyman": [
+        
+            ],
+            "expert": [
+        
+            ],
+            "master": [
+        
+            ]
+        }
+    }
+
+Add trades as you like.
+
+### Adding trades to villagers
+Create a datapack with this directory structure:
+
+    data/villagersplus/villager_trades
+
+Create a JSON with the name of villager-profession to whose trades you'd like to add trades. <br>
+For example:
+
+    horticulturist.json
+
+Below is an example for adding a sell-trade. One diamond block is sold for 12 emeralds.
+
+    {
+        "profession": "villagersplus:horticulturist",
+        "trades": {
+            "novice": [
+                {
+                "type": "villagersplus:sell_item",
+                    "sell": {
+                    "item": "diamond_block",
+                    "count": 1
+                },
+                "priceIn": {
+                    "item": "emerald",
+                    "count": 12
+                },
+                "max_uses": 12,
+                "villager_experience": 4
+                }
+            ]
+        }
+    }
+
+The following trade-types are available for now:
+- buy item
+- process item
+- sell enchanted book
+- sell enchanted tool
+- sell item
+- sell potion
+- sell structure map
+
+An example for each of those is located in:
+
+    data/villagersplus/available_trade_examples
--- src/main/java/com/finallion/villagersplus/trade_offer/TradeOfferResourceListener.java
+++ src/main/java/com/finallion/villagersplus/trade_offer/TradeOfferResourceListener.java
@@ -1,18 +1,22 @@
 package com.finallion.villagersplus.trade_offer;
 
 import com.finallion.villagersplus.VillagersPlus;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
+import com.finallion.villagersplus.trade_offer.tradeOffers.JsonTradeOffer;
+import com.google.gson.*;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
-import net.minecraft.resource.JsonDataLoader;
-import net.minecraft.resource.ResourceManager;
+import net.minecraft.registry.Registries;
+import net.minecraft.resource.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TradeOfferResourceListener extends JsonDataLoader implements IdentifiableResourceReloadListener {
@@ -28,6 +32,16 @@ public class TradeOfferResourceListener extends JsonDataLoader implements Identi
 
     @Override
     protected void apply(Map<Identifier, JsonElement> loader, ResourceManager manager, Profiler profiler) {
+        for (ResourcePack pack : manager.streamResourcePacks().collect(Collectors.toList())) {
+            try {
+                loadAndMergeTradeOffers(pack);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        /*
         loader.forEach((identifier, jsonElement) -> {
             if (!jsonElement.isJsonObject()) {
                 return;
@@ -55,5 +69,42 @@ public class TradeOfferResourceListener extends JsonDataLoader implements Identi
                 defaultProfessionTrades.put(level, mergedTrades);
             });
         });
+
+         */
     }
+
+    private void loadAndMergeTradeOffers(ResourcePack pack) throws IOException {
+        InputSupplier<InputStream> tradeInputStreamSupplier = pack.open(ResourceType.SERVER_DATA, new Identifier(VillagersPlus.MOD_ID, "villager_trades/horticulturist.json"));
+
+        if (tradeInputStreamSupplier == null || tradeInputStreamSupplier.get() == null) {
+            return;
+        }
+
+        JsonObject tradeData = new Gson().fromJson(new InputStreamReader(tradeInputStreamSupplier.get()), JsonObject.class);
+        TradeOfferManager.deserializeJson(tradeData);
+
+        // trades from datapacks
+        Map<VillagerProfession, Int2ObjectOpenHashMap<TradeOffers.Factory[]>> datapackTrades = TradeOfferRegistryLoader.getRegistryForLoading();
+
+        // merge trades added from datapack with the default trades added via data-json
+        TradeOffers.PROFESSION_TO_LEVELED_TRADE.forEach((profession, defaultProfessionTrades) -> {
+            Int2ObjectOpenHashMap<TradeOffers.Factory[]> datapackProfessionTrades = datapackTrades.getOrDefault(profession, new Int2ObjectOpenHashMap<>());
+
+            datapackProfessionTrades.forEach((level, datapackLevelTrades) -> { // collect trades per level and per profession from the datapack
+                TradeOffers.Factory[] defaultLevelTrades = defaultProfessionTrades.get(level);
+
+                TradeOffers.Factory[] mergedTrades = Stream.concat( // if default trades are available, merge them with the datapack trades
+                                (defaultLevelTrades != null) ? Arrays.stream(defaultLevelTrades) : Stream.empty(),
+                                Arrays.stream(datapackLevelTrades))
+                        .distinct()
+                        .toArray(TradeOffers.Factory[]::new);
+
+                defaultProfessionTrades.put(level, mergedTrades);
+            });
+        });
+
+        var x = TradeOffers.PROFESSION_TO_LEVELED_TRADE;
+        var z = 1;
+    }
+
 }
